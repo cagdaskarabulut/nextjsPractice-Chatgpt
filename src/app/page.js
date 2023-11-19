3ce07e15ba5d33c72af1b9d5107a2a2f48796d3e
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-11-20 01:05:19 +0300

    example completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [result, setResult] = useState();
+
+  async function findAnswer(question) {
+    await fetch("/api/chat-gpt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: question,
+      }),
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        let response = Object.values(resData.choices);
+        let onlyFirstResponse = response[0]?.message?.content;
+        setResult(onlyFirstResponse);
+        console.log(onlyFirstResponse);
+      });
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <button
+        onClick={async () =>
+          findAnswer(`Prepare an article for the page created for hero Aatrox's costume named "Justicar Aatrox" on the site developed for the League of Legends game. The article should not have a title. At the beginning of this article, if you know, write in bold how many RP the costume cost and on which date the costume was released, but if you do not know, do not write this sentence at all. In the content of this article, I will talk about the story of Aatrox's "Justicar Aatrox" costume and all the details of the costume in detail, and then make a short narrative about the hero's story, listing his strengths and weaknesses, his past in the game, his place in the game, and then write that you can access information and videos about all the appearances of our hero on this page. end by stating. The article must be in English and consist of at least 1000 words. Return with html tags in div format without style.`)
+        }
+      >
+        Hit API
+      </button>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div dangerouslySetInnerHTML={ {__html: result} } />
     </main>
-  )
+  );
 }
